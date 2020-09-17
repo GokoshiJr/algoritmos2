05ec0c0527687880783373e85a6929085b3a7738
GokoshiJr <julioagonzalez18@gmail.com>
2020-09-16 23:03:35 -0400

    repetido finish

--- src/algoritmos-ordenacion/propuestos/repetido.py
+++ src/algoritmos-ordenacion/propuestos/repetido.py
@@ -2,7 +2,16 @@
 # Por ejemplo si toma los valores [4,7,11,4,9,5,11,7,3,5]
 # Ha de cambiarse a [4,7,11,9,5,3]
 
-numeros = [4, 7, 11, 4, 9, 5, 11, 7, 3, 5]
-
 def eliminar(numArray: list) -> list:
+  largo = len(numArray)
+  unicos = list()
+  for i in range ((largo - 1), -1, -1):
+    if (numArray[i] not in unicos):
+      unicos.append(numArray[i])
+    else:
+      numArray.remove(numArray[i])
   return numArray
+
+if __name__ == "__main__":
+  numeros = [4, 7, 11, 4, 9, 5, 11, 7, 3, 5]
+  print(eliminar(numeros))
